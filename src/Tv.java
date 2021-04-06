f1ea48eccacc9b471d72a64ee31d87a9a1aae616
Frank Greco <fgreco@javasig.com>
2021-04-06 18:10:31 -0400

    New TV file

---
+++ src/Tv.java
@@ -0,0 +1,16 @@
+public class Tv extends ServiceCommunicator{
+    /*
+         iTunesTool - contacts iTunes server and returns artist information.
+    */
+    public Tv (String showname) {
+        super("http://api.tvmaze.com/singlesearch/shows?q=" + showname);
+    }
+
+    /*
+        Main method - just tests this class in isolation.  Normally you would have another class call iTunesTool
+    */
+        public static void main(String[] argv) {
+            System.out.println(new Tv("star trek").get());
+            System.out.println(new Tv("chappell").get());
+        }
+}
